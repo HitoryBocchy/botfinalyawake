f9876e0ff19f17e4a6b02729af69af1ee32ea89d
Chernicow Alex <dariumaster@gmail.com>
2025-04-16 09:59:23 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -9,6 +9,11 @@ bot = Bot(token=TOKEN)
 
 async def main():
     await dp.start_polling(bot)
-
+    
+# Проверяем, является ли данный файл основным модулем
 if __name__ == "__main__":
+     # Запускаем основную асинхронную функцию
     asyncio.run(main())
+except KeyboardInterrupt:
+        # Обрабатываем прерывание программы (например, при нажатии Ctrl+C)
+        print('Бот выключен')
